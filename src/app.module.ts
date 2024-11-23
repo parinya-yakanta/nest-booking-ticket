687caf1ccb946e7901a5d528000ca73b4bedd07c
Parinya Yakanta <yakanta.k@gmail.com>
2024-11-24 04:38:07 +0700

    add middlewares

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { UsersModule } from './modules/users/users.module';
 import { AccessTokensModule } from './modules/access-tokens/access-tokens.module';
@@ -8,6 +8,7 @@ import { ProjectsModule } from './modules/projects/projects.module';
 import { BookingsModule } from './modules/bookings/bookings.module';
 import { OrdersModule } from './modules/orders/orders.module';
 import { LoggerModule } from 'nestjs-pino';
+import { HeadersMiddleware } from './middlewares/headers.middleware';
 
 @Module({
   imports: [
@@ -36,4 +37,10 @@ import { LoggerModule } from 'nestjs-pino';
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(HeadersMiddleware)
+      .forRoutes('*');
+  }
+}
